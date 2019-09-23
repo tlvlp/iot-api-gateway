0d00b4b654a00b10bf498a229ad3d07774850d79
Peter Veres <tllvllp@gmail.com>
2019-09-23 21:40:07 +0200

    Added API and handler service for incoming MQTT messages fixed and simplified
    Signed-off-by: Peter Veres <tllvllp@gmail.com>

--- src/main/java/com/tlvlp/iot/server/api/gateway/rpc/InternalHooksAPI.java
+++ src/main/java/com/tlvlp/iot/server/api/gateway/rpc/InternalHooksAPI.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -20,12 +19,8 @@ public class InternalHooksAPI {
     }
 
     @RequestMapping("${API_GATEWAY_API_INCOMING_MQTT_MESSAGE}")
-    public ResponseEntity handleIncomingMQTTMessage(@RequestBody Map message) {
-        try {
-            internalHooksService.handleIncomingMQTTMessage(message);
-            return new ResponseEntity(HttpStatus.ACCEPTED);
-        } catch (IllegalArgumentException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+    public ResponseEntity handleIncomingMQTTMessage(@RequestBody Map<String, Object> message) {
+        internalHooksService.handleIncomingMQTTMessage(message);
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/tlvlp/iot/server/api/gateway/services/InternalHooksService.java
+++ src/main/java/com/tlvlp/iot/server/api/gateway/services/InternalHooksService.java
@@ -1,11 +1,13 @@
 package com.tlvlp.iot.server.api.gateway.services;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import com.tlvlp.iot.server.api.gateway.config.Properties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Service
 public class InternalHooksService {
@@ -18,36 +20,36 @@ public class InternalHooksService {
         this.restTemplate = restTemplate;
     }
 
-    public void handleIncomingMQTTMessage(Map<String, Object> message) {
-        HashMap response = forwardMessageToUnitService(message);
-        String messageType = (String) response.get("type");
+    public void handleIncomingMQTTMessage(Object message) {
+        DocumentContext processedMessage = JsonPath.parse(forwardMessageToUnitService(message));
+        var messageType = processedMessage.read("$.type", String.class);
         switch (messageType) {
             case "error":
-                // Notify subscribers
-                break;
             case "inactive":
-                // Notify subscribers
+                Object unit = processedMessage.read("$.object");
+                notifySubscribers(unit);
                 break;
             case "status":
-                forwardUnitModuleUpdatesToReporting((UnitModules) response.get("object"));
-                // Notify subscribers
+                List unitModules = processedMessage.read("$.object.modules");
+                forwardUnitModuleUpdatesToReporting(unitModules);
+                notifySubscribers(processedMessage.read("$.object"));
                 break;
         }
 
     }
 
-    private HashMap forwardMessageToUnitService(Map<String, Object> message) {
+    private String forwardMessageToUnitService(Object message) {
         return restTemplate.postForObject(
                 String.format("http://%s:%s%s",
                         properties.getUNIT_SERVICE_NAME(),
                         properties.getUNIT_SERVICE_PORT(),
                         properties.getUNIT_SERVICE_API_INCOMING_MESSAGE()),
                 message,
-                HashMap.class
+                String.class
         );
     }
 
-    private HashMap forwardUnitModuleUpdatesToReporting(UnitModules unitModules) {
+    private HashMap forwardUnitModuleUpdatesToReporting(List unitModules) {
         return restTemplate.postForObject(
                 String.format("http://%s:%s%s",
                         properties.getREPORTING_SERVICE_NAME(),
@@ -57,4 +59,9 @@ public class InternalHooksService {
                 HashMap.class
         );
     }
+
+    private void notifySubscribers(Object unitLog) {
+        //TODO Implement subscriber notification
+    }
+
 }
