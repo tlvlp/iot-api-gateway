0e5d98c810b15c5874a49429d0cd008145c25134
Peter Veres <tllvllp@gmail.com>
2019-09-27 01:20:38 +0200

    Added null checks and exception handling for the Gateway service

--- src/main/java/com/tlvlp/iot/server/api/gateway/services/GatewayService.java
+++ src/main/java/com/tlvlp/iot/server/api/gateway/services/GatewayService.java
@@ -5,9 +5,11 @@ import com.jayway.jsonpath.PathNotFoundException;
 import com.tlvlp.iot.server.api.gateway.config.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -84,92 +86,127 @@ public class GatewayService {
     }
 
     public ResponseEntity requestGlobalStatus() {
-        Object statusRequestMessage = restTemplate.getForEntity(
-                String.format("http://%s:%s%s",
-                        properties.getUNIT_SERVICE_NAME(),
-                        properties.getUNIT_SERVICE_PORT(),
-                        properties.getUNIT_SERVICE_API_REQUEST_GLOBAL_STATUS()),
-                String.class);
-        return restTemplate.postForEntity(
-                String.format("http://%s:%s%s",
-                        properties.getMQTT_CLIENT_SERVICE_NAME(),
-                        properties.getMQTT_CLIENT_SERVICE_PORT(),
-                        properties.getMQTT_CLIENT_API_OUTGOING_MESSAGE()),
-                statusRequestMessage,
-                String.class);
+        try {
+            Object statusRequestMessage = restTemplate.getForEntity(
+                    String.format("http://%s:%s%s",
+                            properties.getUNIT_SERVICE_NAME(),
+                            properties.getUNIT_SERVICE_PORT(),
+                            properties.getUNIT_SERVICE_API_REQUEST_GLOBAL_STATUS()),
+                    String.class);
+            if (statusRequestMessage == null) {
+                throw new IllegalStateException("Unable to get status request message from Unit service.");
+            }
+            return restTemplate.postForEntity(
+                    String.format("http://%s:%s%s",
+                            properties.getMQTT_CLIENT_SERVICE_NAME(),
+                            properties.getMQTT_CLIENT_SERVICE_PORT(),
+                            properties.getMQTT_CLIENT_API_OUTGOING_MESSAGE()),
+                    statusRequestMessage,
+                    String.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Cannot send global status request message: %s", e.getMessage()));
+        }
     }
 
     public ResponseEntity controlUnitModule(Object updatedModule) {
-        Object moduleControlMessage = restTemplate.postForEntity(
-                String.format("http://%s:%s%s",
-                        properties.getUNIT_SERVICE_NAME(),
-                        properties.getUNIT_SERVICE_PORT(),
-                        properties.getUNIT_SERVICE_API_MODULE_CONTROL()),
-                updatedModule,
-                String.class);
-        return restTemplate.postForEntity(
-                String.format("http://%s:%s%s",
-                        properties.getMQTT_CLIENT_SERVICE_NAME(),
-                        properties.getMQTT_CLIENT_SERVICE_PORT(),
-                        properties.getMQTT_CLIENT_API_OUTGOING_MESSAGE()),
-                moduleControlMessage,
-                String.class);
+        try {
+            Object moduleControlMessage = restTemplate.postForEntity(
+                    String.format("http://%s:%s%s",
+                            properties.getUNIT_SERVICE_NAME(),
+                            properties.getUNIT_SERVICE_PORT(),
+                            properties.getUNIT_SERVICE_API_MODULE_CONTROL()),
+                    updatedModule,
+                    String.class);
+            if (moduleControlMessage == null) {
+                throw new IllegalStateException("Unable to get module control message from Unit service.");
+            }
+            return restTemplate.postForEntity(
+                    String.format("http://%s:%s%s",
+                            properties.getMQTT_CLIENT_SERVICE_NAME(),
+                            properties.getMQTT_CLIENT_SERVICE_PORT(),
+                            properties.getMQTT_CLIENT_API_OUTGOING_MESSAGE()),
+                    moduleControlMessage,
+                    String.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Cannot send Unit module control message: %s", e.getMessage()));
+        }
     }
 
     public ResponseEntity addScheduledEventToUnit(Map<String, Object> scheduledEventDetails) {
-        var unitID = scheduledEventDetails.get("unitID");
-        var scheduledEvent = scheduledEventDetails.get("event");
-        String eventID = restTemplate.postForObject(
-                String.format("http://%s:%s%s",
-                        properties.getSCHEDULER_SERVICE_NAME(),
-                        properties.getSCHEDULER_SERVICE_PORT(),
-                        properties.getSCHEDULER_SERVICE_API_POST_EVENT()),
-                scheduledEvent,
-                String.class);
-        return restTemplate.postForEntity(
-                String.format("http://%s:%s%s",
-                        properties.getUNIT_SERVICE_NAME(),
-                        properties.getUNIT_SERVICE_PORT(),
-                        properties.getUNIT_SERVICE_API_ADD_SCHEDULED_EVENT()),
-                Map.of("unitID", unitID,
-                        "eventID", eventID),
-                String.class);
+        try {
+            var unitID = scheduledEventDetails.get("unitID");
+            var scheduledEvent = scheduledEventDetails.get("event");
+            String eventID = restTemplate.postForObject(
+                    String.format("http://%s:%s%s",
+                            properties.getSCHEDULER_SERVICE_NAME(),
+                            properties.getSCHEDULER_SERVICE_PORT(),
+                            properties.getSCHEDULER_SERVICE_API_POST_EVENT()),
+                    scheduledEvent,
+                    String.class);
+            if (eventID == null) {
+                throw new IllegalStateException("Unable to create event at Scheduler service.");
+            }
+            return restTemplate.postForEntity(
+                    String.format("http://%s:%s%s",
+                            properties.getUNIT_SERVICE_NAME(),
+                            properties.getUNIT_SERVICE_PORT(),
+                            properties.getUNIT_SERVICE_API_ADD_SCHEDULED_EVENT()),
+                    Map.of("unitID", unitID,
+                            "eventID", eventID),
+                    String.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Cannot create new Scheduled Event for Unit: %s", e.getMessage()));
+        }
     }
 
     public ResponseEntity deleteScheduledEventFromUnit(Map<String, Object> scheduledEventDetails) {
-        var unitID = scheduledEventDetails.get("unitID");
-        var scheduledEvent = scheduledEventDetails.get("event");
-        String eventID = restTemplate.postForObject(
-                String.format("http://%s:%s%s",
-                        properties.getSCHEDULER_SERVICE_NAME(),
-                        properties.getSCHEDULER_SERVICE_PORT(),
-                        properties.getSCHEDULER_SERVICE_API_DELETE_EVENT()),
-                scheduledEvent,
-                String.class);
-        return restTemplate.postForEntity(
-                String.format("http://%s:%s%s",
-                        properties.getUNIT_SERVICE_NAME(),
-                        properties.getUNIT_SERVICE_PORT(),
-                        properties.getUNIT_SERVICE_API_DELETE_SCHEDULED_EVENT()),
-                Map.of("unitID", unitID,
-                        "eventID", eventID),
-                String.class);
+        try {
+            var unitID = scheduledEventDetails.get("unitID");
+            var scheduledEvent = scheduledEventDetails.get("event");
+            String eventID = restTemplate.postForObject(
+                    String.format("http://%s:%s%s",
+                            properties.getSCHEDULER_SERVICE_NAME(),
+                            properties.getSCHEDULER_SERVICE_PORT(),
+                            properties.getSCHEDULER_SERVICE_API_DELETE_EVENT()),
+                    scheduledEvent,
+                    String.class);
+            if (eventID == null) {
+                throw new IllegalStateException("Unable to delete event at Scheduler service.");
+            }
+            return restTemplate.postForEntity(
+                    String.format("http://%s:%s%s",
+                            properties.getUNIT_SERVICE_NAME(),
+                            properties.getUNIT_SERVICE_PORT(),
+                            properties.getUNIT_SERVICE_API_DELETE_SCHEDULED_EVENT()),
+                    Map.of("unitID", unitID,
+                            "eventID", eventID),
+                    String.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Cannot delete Scheduled Event from Unit: %s", e.getMessage()));
+        }
     }
 
-    public ResponseEntity getReportsForUnit(
-            String unitID, String moduleID, LocalDateTime timeFrom, LocalDateTime timeTo,
-            Set<ChronoUnit> requestedScopes) {
-        return restTemplate.getForEntity(
-                String.format("http://%s:%s%s?unitID=%s&moduleID=%s&timeFrom=%s&timeTo=%s&requestedScopes=%s",
-                        properties.getREPORTING_SERVICE_NAME(),
-                        properties.getREPORTING_SERVICE_PORT(),
-                        properties.getREPORTING_SERVICE_API_GET_AVERAGES(),
-                        unitID,
-                        moduleID,
-                        timeFrom,
-                        timeTo,
-                        requestedScopes),
-                String.class
-        );
+    public ResponseEntity getReportsForUnit(String unitID, String moduleID, LocalDateTime timeFrom,
+                                            LocalDateTime timeTo, Set<ChronoUnit> requestedScopes){
+        try {
+            return restTemplate.getForEntity(
+                    String.format("http://%s:%s%s?unitID=%s&moduleID=%s&timeFrom=%s&timeTo=%s&requestedScopes=%s",
+                            properties.getREPORTING_SERVICE_NAME(),
+                            properties.getREPORTING_SERVICE_PORT(),
+                            properties.getREPORTING_SERVICE_API_GET_AVERAGES(),
+                            unitID,
+                            moduleID,
+                            timeFrom,
+                            timeTo,
+                            requestedScopes),
+                    String.class);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Cannot generate Reports for Unit: %s", e.getMessage()));
+        }
     }
 }
